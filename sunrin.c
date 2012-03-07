c067bc38b182f8c33194c3f52c48529c6d845a36
unknown <Administrator@.(none)>
2012-03-07 22:31:42 +0900

    Lee Hyun Soo

--- sunrin.c
+++ sunrin.c
@@ -4,6 +4,7 @@ int main(int argc, char **argv){
     int tape[99];
     int *head = tape;
     char curMconfig;
+	int a=0,i=0;
 
     FILE *fp;
     fp = fopen(argv[1], "r");
@@ -28,8 +29,12 @@ int main(int argc, char **argv){
             case '-':
                 --*head;
                 break;
+			case '*':
+				scanf("%d",&a);
+				for(i=1;i<=a;i++)
+					printf("*");
+				break;
         }
     }
     fclose(fp);
-
 }
